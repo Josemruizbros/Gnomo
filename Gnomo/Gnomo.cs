54165daaca403ca4a64a987363e7a940f33ef2c3
Josemruizbros <josemanuelruizpalacios1@gmail.com>
2026-02-10 12:12:40 +0100

    Agregar archivos de proyecto.

---
+++ Gnomo/Gnomo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gnomo
+{
+    public class Gnomo
+    {
+        protected String nombre;
+        protected String color;
+    }
+}
---
+++ Gnomo/GnomoLadron.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gnomo;
+
+namespace Gnomo
+{
+    public class GnomoLadron : Gnomo
+    {
+        private String especialidad;
+        public bool robarObjeto(Objeto o)
+        {
+            return true;
+        }
+    }
+}
